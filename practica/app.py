4358cb7a77382d8bb0549e0eb2cb26326eabaaee
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2026-04-07 17:46:32 -0500

    Actualizar el puerto predeterminado de la aplicación Flask a 10000

--- practica/app.py
+++ practica/app.py
@@ -1769,6 +1769,6 @@ def download():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
+    port = int(os.environ.get("PORT", "10000"))
     debug = os.environ.get("FLASK_DEBUG", "").lower() in {"1", "true", "yes", "on"}
     app.run(host="0.0.0.0", port=port, debug=debug)
